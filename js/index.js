6660f827c6f35e45329ce487ac402f3f66cca876
mauricebuchi <mauricejahwizwid@gmail.com>
2021-10-26 10:51:59 +0100

    remove php and add appear on scroll

--- js/index.js
+++ js/index.js
@@ -1,29 +1,50 @@
-const faders = document.querySelectorAll(".fade-in");
-const sliders = document.querySelectorAll(".slide-in");
+// const faders = document.querySelectorAll(".fade-in");
+// const sliders = document.querySelectorAll(".slide-in");
 
-const appearOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px -250px 0px",
+// const appearOptions = {
+//   threshold: 0,
+//   rootMargin: "0px 0px -250px 0px",
+// };
+// const appearOnScroll = new IntersectionObserver(function (
+//   entries,
+//   appearOnScroll
+// ) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     } else {
+//       entry.target.classList.add("appear");
+//       appearOnScroll.unobserve(entry.target);
+//     }
+//   });
+// },
+// appearOptions);
+
+// faders.forEach((fader) => {
+//   appearOnScroll.observe(fader);
+// });
+
+// sliders.forEach((slider) => {
+//   appearOnScroll.observe(slider);
+// });
+
+//========================== For the reveal on scroll=====================================
+// Real all sections
+const allSections = document.querySelectorAll(".scroll");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
 };
-const appearOnScroll = new IntersectionObserver(function (
-  entries,
-  appearOnScroll
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-},
-appearOptions);
 
-faders.forEach((fader) => {
-  appearOnScroll.observe(fader);
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.2,
 });
 
-sliders.forEach((slider) => {
-  appearOnScroll.observe(slider);
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
 });
--- php/mail.php
+++ php/mail.php
@@ -1,22 +1,22 @@
 <?php
 //   get data
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $comment = $_POST['comment'];
+// if (isset($_POST['submit'])) {
+//     $name = $_POST['name'];
+//     $email = $_POST['email'];
+//     $phone = $_POST['phone'];
+//     $comment = $_POST['comment'];
 
-    $to = "mauricejahwizwid@gmail.com";
-    $subject = "Mail from Enyems Enterprise";
-    $txt = "Name =" .$name . "\r\nEmail = " .$email . "\r\nPhone Number = " . $phone ."\r\nComments = " .$comment;
-    $headers = "From: noreply@Eyems Enterprise.com" . "\r\n";
-    if ($email != null) {
-        mail($to, $subject, $txt, $headers);
-        header("about.html");
-    }
+//     $to = "mauricejahwizwid@gmail.com";
+//     $subject = "Mail from Enyems Enterprise";
+//     $txt = "Name =" .$name . "\r\nEmail = " .$email . "\r\nPhone Number = " . $phone ."\r\nComments = " .$comment;
+//     $headers = "From: noreply@Eyems Enterprise.com" . "\r\n";
+//     if ($email != null) {
+//         mail($to, $subject, $txt, $headers);
+//         header("about.html");
+//     }
 
    
-}
+// }
 
 ?>
